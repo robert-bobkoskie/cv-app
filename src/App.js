4176c0dd6ab70d9ed0cf7bb15d1be9fb7f14b16e
Robert <robert.bobkoskie@gmail.com>
2022-01-21 13:22:30 -0500

    Mod react app: Center images

--- src/App.js
+++ src/App.js
@@ -28,6 +28,15 @@ import P2_CAR_LIGHTENING_X_TESTGif from './images/PHASE_2/CAR_LIGHTENING_X_TEST.
 import P2_CAR_BAD_BLEND_LIGHTENINGGif from './images/PHASE_2/CAR_BAD_BLEND_LIGHTENING.gif';
 import FINAL_CAR_GOOD_BLEND from './images/PHASE_2/FINAL_CAR_GOOD_BLEND.gif';
 
+/*	===============================================
+	// Use npm to start application from working dir:
+	$ pwd
+	/c/Users/rbobkoskie/Documents/GIT/robert-bobkoskie/cv-app
+	$ npm start
+	
+	// Use npm run deploy to push updates to github server:
+	$ npm run deploy
+	=============================================== */
 
 function MyComponent(props) {
 	const vidRef = useRef(null);
@@ -92,21 +101,31 @@ function App() {
 					of the mask). My first video used a street scene as the "dark" image, and the generated mask(s) and
 					"white" image(s) to create the novel video. To create the final gif, I used GIMP to resize and relocate
 					the Cartoon gif. I blended the images using the same code.
-					<br /><br />
-					
+				</p>
+				<br /><br />
+				
+				<div className="centered_img">
 					<img src={P1_FinalGif} className="img_1" alt="Phase 1: Deliverable" />
-					<br /><br />
+				</div>
+				
+				<p className="text_1">
 					Phase 1 Deliverable (above): gif of street scene blended with cartoon gif and moving car.
-					<br /><br />
-					
+				</p>
+				<br /><br />
+				
+				<div className="centered_img">
 					<img style = {{ paddingRight:10 }} src={P1_RawGif} className="img_2" alt="Phase 1: Video texture created from MP4" />
 					<img style = {{ paddingRight:10 }} src={P1_CarIdentifiedGif} className="img_2" alt="Phase 1: Car Identified" />
 					<img src={P1_CarMaskGif} className="img_2" alt="Phase 1: Mask of Car" />
-					<br /><br />
+				</div>
+				
+				<p className="text_1">
 					Above from left: Video texture created from MP4, Object recognition (car) in video texture, Video tracking, create
 					mask of car for image blending in video texture.
-					<br /><br />
-					
+				</p>
+				<br /><br />
+				
+				<p className="text_1">
 					<div className="row">
 						<img style = {{ paddingRight:10 }} src={P1_CarStreetSceneGif} className="img_4" alt="Phase 1: Car Identified" />
 						<div style = {{ width:200, paddingRight:10 }} >
@@ -115,8 +134,8 @@ function App() {
 						</div>
 						<img src={P1_Simpsons_60fpsGif} className="img_4" alt="Phase 1: Car Identified" />
 					</div>
-					<br /><br />
 				</p>
+				<br /><br />
 				<br /><br />
 				
 				<h1>Phase 2</h1>
@@ -126,46 +145,62 @@ function App() {
 					To create the final gifs, I used a code based approach to auto-size, auto-detect images in frames; generating masks
 					and "white" images for blending. The output (a gif like the doorway + lightening) became the input for creating a
 					novel gif. This approach was used recursively four times to create the final gif of the disappearing car.
-					<br /><br />
-					
+				</p>
+				<br /><br />
+				
+				<div className="centered_img">
 					<img style = {{ paddingRight:10 }} src={P2_STREETwithDOORGif} className="img_2" alt="Phase 2: Street scene with door" />
 					<img style = {{ paddingRight:10 }} src={P2_CAR_DISAPPEARGif} className="img_2" alt="Phase 2: Car Disappear" />
 					<img src={P2_CarFinalDevGif} className="img_Full" className="img_2" alt="Phase 2: Final Deliverable" />
-					<br /><br />
+				</div>
+				
+				<p className="text_1">
 					Above from Left: Doorway + lightening blended with street scene, Car with good blending into street scene, Disappearing car.
 					After some experimentation, these gifs would eventually comprise the final gif (above right and top of page).
-					<br /><br />
-					
-					<img style = {{ paddingRight:10 }} src={P2_LIGHTENING_FULL_SIZEGif} className="img_3" alt="Phase 2: Street scene with door" />
+				</p>
+				<br /><br />
+				
+				<div className="centered_img">
+					<img style = {{ paddingRight:10 }} src={P2_LIGHTENING_FULL_SIZEGif} className="img_3" alt="Phase 2: lightening" />
 					<img style = {{ paddingRight:10 }} src={P2_DOOR_Gif} className="img_3" alt="Phase 2: Door" />
 					<img style = {{ paddingRight:10 }} src={P2_DOOR_MARKEDGif} className="img_3" alt="Phase 2: Door with mask" />
 					<img style = {{ paddingRight:10 }} src={P2_LIGHTENING_PLUS_DOORGif} className="img_3" alt="Phase 2: Door with lightening" />
-					<br /><br />
+				</div>
+				
+				<p className="text_1">
 					Above from Left: Lightening gif, Doorway gif, Open doorway detected, Open doorway blended with lightening.
-					<br /><br />
-					
+				</p>
+				<br /><br />
+				
+				<div className="centered_img">
 					<img style = {{ paddingRight:10 }} src={P2_CAR_STREET_X_TESTGif} className="img_4" alt="Phase 2: Car with street X test" />
 					<img style = {{ paddingRight:10 }} src={P2_CAR_LIGHTENING_X_TESTGif} className="img_4" alt="Phase 2: Car with lightening X test" />
 					<br /><br />
+				</div>
+				
+				<p className="text_1">
 					Above, the “X” experiments demonstrate image masking and blending. The gif on the left was marked with “X”s about both the
 					car and buildings. You’ll need to observe the gifs closely, as the “x”s appear as a “flicker” in the gifs. The car in this
 					image is associated with the “white” area of the mask (from Phase 1 development), everything else, including the buildings
 					with the “dark” area. Thus, the buildings would get masked out of the output gif and replaced with the street scene from the
 					gif with the doorway and lightening. The gif on the right is the output, a blend of gif on the left with the gif of the street
 					scene + doorway + lightening (above left). As expected only the 'X's near the car appear in the final output.
-					<br /><br />
-					
+				</p>
+				<br /><br />
+				
+				<div className="centered_img">
 					<img style = {{ paddingRight:10 }} src={P2_CAR_BAD_BLEND_LIGHTENINGGif} className="img_4" alt="Phase 2: Car with bad blending" />
 					<img style = {{ paddingRight:10 }} src={FINAL_CAR_GOOD_BLEND} className="img_4" alt="Phase 2: Car with good blending" />
-					<br /><br />
+				</div>
+				
+				<p className="text_1">
 					Above, Blending experiments: The gif on the left is blended with a "white" mask area that is the same shape as the car.
 					The gif on the right is blended with a slightly smaller area, by seven pixels on both axis [h, w] for the "white" mask. This small
 					reduction in the shape of the mask appears to have improved the blending at the edges of the car, while not
 					masking out too much of the car. Notice that some of the front tire in the blended image is masked out with the smaller mask.
 					Using a "white" mask that was much smaller (~15 pixels, [h, w]) would mask out too much of the car and, thus was not a consideration.
-					<br /><br />
-					
 				</p>
+				<br /><br />	
 			</div>
 		</div>
 	);
